ec43870d3faab178c15b7bec3869c1d900b2aa61
Aditya Matsa <aditya.matsa@icloud.com>
2024-05-17 16:09:59 +0530

    demo: toggle favourite courses indicator by clicking on button

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-const myDiv = document.createElement("div");
+// Select the correct location where the button will be inserted
+const buttonLocation = document.querySelectorAll(".card--nanodegree__title");
 
-function listener (e) {
-    if (e.target.nodeName.toLowerCase() === "p")
-    console.log("A paragraph was clicked: " + e.target.textContent);
+// Listen for click events and toggle favorite status
+function clickListener (event) {
+    event.preventDefault();
+    if (event.target.textContent.includes("Add")) {
+        // Change button text and add indicator
+        event.target.textContent = "Remove from Favourites";
+        const star = document.createElement("div");
+        star.textContent = "⭐️⭐️⭐️"
+        event.target.parentElement.appendChild(star);
+    } else {
+        // Bring button back to original state
+        event.target.textContent = "Add to Favourites";
+        event.target.nextSibling.remove();
+    }
 }
 
-for (let i = 1; i <= 200; i++) {
-    const newEl = document.createElement("p");
-    newEl.textContent = `This is paragraph number ${i}`;
-    myDiv.appendChild(newEl);
-}
+for (let i = 0; i < buttonLocation.length; i++) {
+    // Create a button, add text and styles to it
+    const myButton = document.createElement("button");
+    myButton.textContent = "Add to Favourites";
+    myButton.classList.add("button", "button--primary");
 
-myDiv.addEventListener("click", listener);
-document.body.appendChild(myDiv);
+    // Add event listener for click events and fire clickListener
+    myButton.addEventListener("click", clickListener);
+
+    // Append the button in all required locations
+    buttonLocation[i].appendChild(myButton);
+}
