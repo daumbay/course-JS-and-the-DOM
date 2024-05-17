f4b6259391d4a5bf3e98b77163477f38d072108d
Aditya Matsa <aditya.matsa@icloud.com>
2024-05-17 14:37:34 +0530

    demo: event delegation using Event.target

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-const link = document.querySelector("#my-classroom");
-document.addEventListener("click", function(e) {
-    e.preventDefault();
-    console.log("Wow! We didn't navigate to a new page!");
-});
+const myDiv = document.createElement("div");
+
+function listener (e) {
+    if (e.target.nodeName.toLowerCase() === "p")
+    console.log("A paragraph was clicked: " + e.target.textContent);
+}
+
+for (let i = 1; i <= 200; i++) {
+    const newEl = document.createElement("p");
+    newEl.textContent = `This is paragraph number ${i}`;
+    myDiv.appendChild(newEl);
+}
+
+myDiv.addEventListener("click", listener);
+document.body.appendChild(myDiv);
